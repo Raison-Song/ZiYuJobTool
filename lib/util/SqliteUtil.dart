7a9a34ca9156c2a35d38138990e65e0cb01f6ecf
Raison-Song <syhsuiyue@gmail.com>
2023-04-24 20:44:07 +0800

    更新sql创建

--- lib/util/SqliteUtil.dart
+++ lib/util/SqliteUtil.dart
@@ -19,40 +19,71 @@ class DBManager {
 
     return openDatabase(
       path.join(await getDatabasesPath(), 'identifier.sqlite'),
-      onCreate: (db, version) async  {
+      onCreate: (db, version) async {
         //如果数据库不存在则创建
         return db.execute("""
-        create table if not exists dogs (
-          id INTEGER PRIMARY KEY autoincrement,
-          `name` TEXT NOT NULL
+        create table if not exists clipboard_txt
+        (
+            id         text              not null,
+            user_id    text              not null,
+            is_collect integer default 0 not null,
+            txt        text              not null
         );
-
-        create table if not exists users (
-          id TEXT NOT NULL,
-          token TEXT NOT NULL,
-          is_use INT NOT NULL
+        
+        create table if not exists folder
+        (
+            id                 TEXT not null
+                constraint pk
+                    primary key,
+            folder_name        TEXT not null,
+            before_folder_name TEXT not null,
+            group_name         TEXT not null,
+            user_id            text not null
         );
-
-        create table if not exists groups (
-          id TEXT NOT NULL CONSTRAINT pk PRIMARY KEY,
-          user_id TEXT NOT NULL,
-          group_name TEXT NOT NULL
+        
+        create table if not exists folder_file
+        (
+            id          TEXT                      not null
+                constraint pk
+                    primary key,
+            user_id     TEXT                      not null,
+            group_name  TEXT                      not null,
+            file_name   TEXT                      not null,
+            folder_name text                      not null,
+            upload_time integer default TIMESTAMP not null
         );
-
-        create table if not exists folder_file (
-          id TEXT NOT NULL CONSTRAINT pk PRIMARY KEY,
-          user_id TEXT NOT NULL,
-          group_id TEXT NOT NULL,
-          file_name TEXT NOT NULL
+        
+        create table if not exists groups
+        (
+            id         text not null
+                constraint pk
+                    primary key,
+            user_id    text not null,
+            group_name text not null
         );
-
-        CREATE UNIQUE INDEX if not exists READ ON folder_file (group_name);
-
-        create table if not exists folder (
-          id TEXT NOT NULL CONSTRAINT pk PRIMARY KEY,
-          folder_name TEXT NOT NULL,
-          before_folder_name TEXT NOT NULL
+        
+        create table if not exists sqlite_master
+        (
+            type     TEXT,
+            name     TEXT,
+            tbl_name TEXT,
+            rootpage INT,
+            sql      TEXT
         );
+        
+        create table if not exists sqlite_sequence
+        (
+            name,
+            seq
+        );
+        
+        create table if not exists users
+        (
+            id     TEXT not null,
+            token  TEXT not null,
+            is_use int  not null
+        );
+        
        """);
       },
       version: 3,
@@ -62,5 +93,4 @@ class DBManager {
   Future<Database> getDatabase() async {
     return await initDBManager();
   }
-
 }
